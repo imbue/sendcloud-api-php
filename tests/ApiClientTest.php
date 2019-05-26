b3c7229f42ab46d6d29b4c69ebe0f4adc6e01450
imbue <liamboer@outlook.com>
2019-05-26 19:56:42 +0200

    Added basic unit test

---
+++ tests/ApiClientTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\SendCloud;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+use Imbue\SendCloud\SendCloudApiClient;
+
+class ApiClientTest extends TestCase
+{
+    /** @var ClientInterface */
+    private $guzzleClient;
+    /** @var SendCloudApiClient */
+    private $sendCloudApiClient;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->guzzleClient = $this->createMock(Client::class);
+        $this->sendCloudApiClient = new SendCloudApiClient($this->guzzleClient);
+        $this->sendCloudApiClient->setApiAuth('test_api_key', 'test_api_secret');
+    }
+
+    public function testPerformHttpCallReturnsBodyAsObject()
+    {
+        $response = new Response(200, [], '{"object": "parcel"}');
+
+        $this->guzzleClient
+            ->expects($this->once())
+            ->method('send')
+            ->willReturn($response);
+
+        $parsedResponse = $this->sendCloudApiClient->performHttpCall('GET', '');
+
+        $this->assertEquals(
+            (object)['object' => 'parcel'],
+            $parsedResponse
+        );
+    }
+}
